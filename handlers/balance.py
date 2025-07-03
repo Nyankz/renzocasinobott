c94f0003cfd4354cea83bc9a09931383aef0800a
Nyankz <zhakefigmm@gmail.com>
2025-07-03 13:36:03 +0500

    Update balance.py

--- handlers/balance.py
+++ handlers/balance.py
@@ -65,12 +65,11 @@ async def show_deposit_invoice(callback: CallbackQuery):
     currency = callback.data.split("_")[1]
     user_id = callback.from_user.id
 
-    invoice = await 
-create_invoice(user_id, currency)
+    invoice = await create_invoice(user_id, currency)
+
     if not invoice or "pay_url" not in invoice:
-    await 
-        callback.answer("Ошибка при создании чека!", show_alert=True)
-    return
+        await callback.answer("Ошибка при создании чека!", show_alert=True)
+        return
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="💸 Перейти к оплате", url=invoice["pay_url"])],
